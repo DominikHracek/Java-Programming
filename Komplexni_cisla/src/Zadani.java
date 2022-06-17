3a6c6a8c125f3d000e37525148b88d0532668326
DominikHracek <60671891+DominikHracek@users.noreply.github.com>
2022-06-17 12:13:47 +0200

    Add files via upload

--- Komplexni_cisla/src/Zadani.java
+++ Komplexni_cisla/src/Zadani.java
@@ -1,75 +1,121 @@
-import java.util.Scanner;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
 
-public class Zadani{
-	
-	Scanner scanner = new Scanner(System.in);
-	
-	
-	
+public class Zadani extends JFrame implements ActionListener{
+
+	JButton ulozit_zadani;
+	JTextField hodnota_R;
+	JTextField zadejte_rc;
+	JTextField hodnota_f;
+	JTextField hodnota_C;
+	JTextField hodnota_L;
+	JTextField hodnota_U;
+	JTextField hodnota_I;
+	JTextField hodnota_rS; 
+
 	Zadani(){
 
-		System.out.println("Zapiste REALNE SLOZKY, pokud neznate, stisknete Enter: ");
-		System.out.println("R: ");
-		String rhodnota_R = scanner.nextLine();
-		System.out.println("f: ");
-		String rhodnota_f = scanner.nextLine();
-		System.out.println("UR: ");
-		String rhodnota_UR = scanner.nextLine();
-		System.out.println("UL: ");
-		String rhodnota_UL = scanner.nextLine();
-		System.out.println("UC: ");
-		String rhodnota_UC = scanner.nextLine();
-		System.out.println("U: ");
-		String rhodnota_U = scanner.nextLine();
-		System.out.println("I: ");
-		String rhodnota_I = scanner.nextLine();
-		System.out.println("IR: ");
-		String rhodnota_IR = scanner.nextLine();
-		System.out.println("IL: ");
-		String rhodnota_IL = scanner.nextLine();
-		System.out.println("IC: ");
-		String rhodnota_IC = scanner.nextLine();
-		System.out.println("S: ");
-		String rhodnota_S = scanner.nextLine();
-		System.out.println("P: ");
-		String rhodnota_P = scanner.nextLine();
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new GridLayout(15,2));
 		
-		System.out.println("Zapiste IMAGINARNI SLOZKY, pokud neznate, stisknete Enter: ");
-		System.out.println("UR: ");
-		String ihodnota_UR = scanner.nextLine();
-		System.out.println("UL: ");
-		String ihodnota_UL = scanner.nextLine();
-		System.out.println("UC: ");
-		String ihodnota_UC = scanner.nextLine();
-		System.out.println("U: ");
-		String ihodnota_U = scanner.nextLine();
-		System.out.println("I: ");
-		String ihodnota_I = scanner.nextLine();
-		System.out.println("IR: ");
-		String ihodnota_IR = scanner.nextLine();
-		System.out.println("IL: ");
-		String ihodnota_IL = scanner.nextLine();
-		System.out.println("IC: ");
-		String ihodnota_IC = scanner.nextLine();
-		System.out.println("S: ");
-		String ihodnota_S = scanner.nextLine();
-		System.out.println("Q: ");
-		String ihodnota_Q = scanner.nextLine();
+		ulozit_zadani = new JButton("Submit");
+		ulozit_zadani.addActionListener(this);
 		
-		System.out.println("Je to správně?");
-		System.out.println("R: ("+rhodnota_R+") Ω");
-		System.out.println("f: ("+rhodnota_f+") Hz");
-		System.out.println("UR: ("+rhodnota_UR+" + "+ihodnota_UR+"j) V");
-		System.out.println("UC: ("+rhodnota_UC+" + "+ihodnota_UC+"j) V");
-		System.out.println("UL: ("+rhodnota_UL+" + "+ihodnota_UL+"j) V");
-		System.out.println("U: ("+rhodnota_U+" + "+ihodnota_U+"j) V");
-		System.out.println("I: ("+rhodnota_I+" + "+ihodnota_I+"j) A");
-		System.out.println("IR: ("+rhodnota_IR+" + "+ihodnota_IR+"j) A");
-		System.out.println("IL: ("+rhodnota_IL+" + "+ihodnota_IL+"j) A");
-		System.out.println("IC: ("+rhodnota_IC+" + "+ihodnota_IC+"j) A");
-		System.out.println("S: ("+rhodnota_S+" + "+ihodnota_S+"j) VA");
-		System.out.println("P: "+rhodnota_P+" W");
-		System.out.println("Q: "+ihodnota_Q+"j) var");
+		zadejte_rc = new JTextField();
+		zadejte_rc.setPreferredSize(new Dimension(1920,1080));
+		zadejte_rc.setFont(new Font("Consolas",Font.PLAIN,35));
+		zadejte_rc.setForeground(Color.white);
+		zadejte_rc.setBackground(Color.black);
+		zadejte_rc.setCaretColor(Color.white);
+		zadejte_rc.setText("Dolů zapište POUZE REÁLNÉ HODNOTY");
+		zadejte_rc.setEditable(false);
 		
+		hodnota_R = new JTextField();
+		hodnota_R.setPreferredSize(new Dimension(1920,1080));
+		hodnota_R.setFont(new Font("Consolas",Font.PLAIN,35));
+		hodnota_R.setForeground(Color.white);
+		hodnota_R.setBackground(Color.black);
+		hodnota_R.setCaretColor(Color.white);
+		hodnota_R.setText("hodnota R:");
+		
+		hodnota_f = new JTextField();
+		hodnota_f.setPreferredSize(new Dimension(1920,1080));
+		hodnota_f.setFont(new Font("Consolas",Font.PLAIN,35));
+		hodnota_f.setForeground(Color.white);
+		hodnota_f.setBackground(Color.black);
+		hodnota_f.setCaretColor(Color.white);
+		hodnota_f.setText("hodnota f:");
+		
+		hodnota_C = new JTextField();
+		hodnota_C.setPreferredSize(new Dimension(1920,1080));
+		hodnota_C.setFont(new Font("Consolas",Font.PLAIN,35));
+		hodnota_C.setForeground(Color.white);
+		hodnota_C.setBackground(Color.black);
+		hodnota_C.setCaretColor(Color.white);
+		hodnota_C.setText("hodnota C:");
+		
+		hodnota_L = new JTextField();
+		hodnota_L.setPreferredSize(new Dimension(1920,1080));
+		hodnota_L.setFont(new Font("Consolas",Font.PLAIN,35));
+		hodnota_L.setForeground(Color.white);
+		hodnota_L.setBackground(Color.black);
+		hodnota_L.setCaretColor(Color.white);
+		hodnota_L.setText("hodnota L:");
+		
+		hodnota_U = new JTextField();
+		hodnota_U.setPreferredSize(new Dimension(1920,1080));
+		hodnota_U.setFont(new Font("Consolas",Font.PLAIN,35));
+		hodnota_U.setForeground(Color.white);
+		hodnota_U.setBackground(Color.black);
+		hodnota_U.setCaretColor(Color.white);
+		hodnota_U.setText("hodnota U:");
+		
+		hodnota_I = new JTextField();
+		hodnota_I.setPreferredSize(new Dimension(1920,1080));
+		hodnota_I.setFont(new Font("Consolas",Font.PLAIN,35));
+		hodnota_I.setForeground(Color.white);
+		hodnota_I.setBackground(Color.black);
+		hodnota_I.setCaretColor(Color.white);
+		hodnota_I.setText("hodnota I:");
+		
+		hodnota_rS = new JTextField();
+		hodnota_rS.setPreferredSize(new Dimension(1920,1080));
+		hodnota_rS.setFont(new Font("Consolas",Font.PLAIN,35));
+		hodnota_rS.setForeground(Color.white);
+		hodnota_rS.setBackground(Color.black);
+		hodnota_rS.setCaretColor(Color.white);
+		hodnota_rS.setText("hodnota S:");
+		
+		this.add(zadejte_rc);
+		this.add(hodnota_R);
+		this.add(hodnota_f);
+		this.add(hodnota_L);
+		this.add(hodnota_C);
+		this.add(hodnota_U);
+		this.add(hodnota_I);
+		this.add(hodnota_rS);
+		this.add(ulozit_zadani);
+		this.pack();
+		this.setVisible(true);
+
+	}
+
+	@Override
+
+	public void actionPerformed(ActionEvent e) {
+
+		if(e.getSource()==ulozit_zadani) {
+			
+			this.dispose();
+			
+		}
+
 	}
 }
